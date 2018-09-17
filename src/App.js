7dcd8fd109ac8a483fac49d2f2b688fea311349b
TedtheDev <tylerjedmunds@gmail.com>
2018-09-17 00:33:52 -0500

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
 import Calculator from './components/Calculator/calculator';
+
+
+const AppDiv = styled.div`
+  display: grid;
+  grid-template: 10% 80% 10% / 10% 80% 10%;
+  grid-template-areas:
+    "header header header"
+    ". content ."
+    "footer footer footer";
+  width: 100%;
+  height: 100%;
+`;
+
+const MainContent = styled.main`
+  grid-area: content;
+  display: flex;
+  justify-content: center;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <main className="App-main">
+      <AppDiv>
+        <MainContent>
           <Calculator />
-        </main>
-      </div>
+        </MainContent>
+      </AppDiv>
     );
   }
 }
